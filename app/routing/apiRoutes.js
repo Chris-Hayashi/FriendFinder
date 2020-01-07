ca0cf951afa550a8500bac387b13522efdcd8353
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-01-06 18:06:25 -0800

    Finished coding survey.html (survey displaying in browser)

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/api/friends", function(req, res) {
+    
+});
+
+router.post("/api/friends", function(req, res) {
+
+});
+
+module.exports = router;
