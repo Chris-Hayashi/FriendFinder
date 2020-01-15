3539187731bd93a98dbae172a0fbbae848538dab
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-01-15 15:29:18 -0800

    removed console.log(s)

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,16 +4,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/api/friends", function(req, res) {
-    // var friends = require("./../data/friends");
     res.json(friends);
 });
 
 router.post("/api/friends", function(req, res) {
-    // var user = req.body.q1;
-    console.log(friends);
-    // console.log(req.body);
-    console.log("POST");
-    // friends.push(user);
+
     var currentDiff = 0;
     var bestMatchDiff = 40; //Maximum possible difference
     var bestMatch;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var path = require("path");
 var router = express.Router();
-// var app = express();
 
 router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
