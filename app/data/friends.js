cb950a882d5d7d154cc75b95aa9cbb3e7d6f66c4
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-01-15 12:57:21 -0800

    Finished coding friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,55 +18,136 @@ var friends = [
         "name" : "Leonardo Dicaprio",
         "photo" : "https://www.onthisday.com/images/people/leonardo-dicaprio-medium.jpg",
         "scores" : [
-
+            1,
+            3,
+            3,
+            5,
+            4,
+            4,
+            2,
+            2,
+            1,
+            3
         ]
     }, {
         "name" : "Morgan Freeman",
         "photo" : "https://cdn.britannica.com/40/144440-050-DA828627/Morgan-Freeman.jpg",
         "scores" : [
-
+            3,
+            4,
+            4,
+            2,
+            2,
+            5,
+            1,
+            3,
+            3,
+            4
         ]
     }, {
         "name" : "Kate Upton",
         "photo" : "https://pbs.twimg.com/profile_images/887828156886986752/F7XIdhSg.jpg",
         "scores" : [
-
+            3,
+            3,
+            3,
+            5,
+            4,
+            1,
+            1,
+            3,
+            2,
+            4
         ]
     }, {
         "name" : "Kevin Hart",
         "photo" : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hart-gettyimages-1062194608mod-1544461779.jpg?crop=1xw:1xh;center,top&resize=480:*",
         "scores" : [
-
+            5,
+            5,
+            4,
+            2,
+            3,
+            3,
+            1,
+            5,
+            4,
+            4
         ]
     }, {
         "name" : "Taylor Swift",
         "photo" : "https://www.rollingstone.com/wp-content/uploads/2019/12/Tay-2.jpg?resize=900,600&w=450",
         "scores" : [
-
+            1,
+            1,
+            2,
+            5,
+            2,
+            3,
+            3,
+            4,
+            2,
+            5
         ]
     }, {
         "name" : "Mila Kunis",
         "photo" : "https://image.insider.com/5d51ab572d4cb57604523c73?width=1100&format=jpeg&auto=webp",
         "scores" : [
-
+            5,
+            5,
+            1,
+            3,
+            3,
+            2,
+            4,
+            4,
+            3,
+            3
         ]
     }, {
         "name" : "Naomi Scott",
         "photo" : "https://m.media-amazon.com/images/M/MV5BMzQ5YWRlYjktZDM5My00N2YwLTg4N2ItYmY2ZDQ4ZGYwMzBkXkEyXkFqcGdeQXVyNDU1NjUxOTc@._V1_.jpg",
         "scores" : [
-
+            5,
+            2,
+            4,
+            4,
+            3,
+            5,
+            5,
+            3,
+            2,
+            2,
         ]
     }, {
         "name" : "Chris Pratt",
         "photo" : "https://upload.wikimedia.org/wikipedia/commons/9/99/Chris_Pratt_2018.jpg",
         "scores" : [
-
+            2,
+            3,
+            2,
+            4,
+            4,
+            5,
+            1,
+            3,
+            3,
+            5
         ]
     }, {
         "name" : "Kendrick Lamar",
         "photo" : "https://www.rap-up.com/app/uploads/2014/12/kendrick-lamar.jpg",
         "scores" : [
-        
+            1,
+            1,
+            5,
+            3,
+            2,
+            4,
+            4,
+            3,
+            3,
+            5
         ]
 }];
 
